7efa64b10d810d70228d79f3441ddd0570558825
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-01 19:56:33 +0700

    [ADD] comments section

---
+++ src/components/Comments/Comment.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable react/prop-types */
+import { Avatar, Divider, Flex, Text } from '@chakra-ui/react'
+import { useState } from 'react'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+
+import { BsThreeDots } from 'react-icons/bs'
+import { FaRegComment } from 'react-icons/fa6'
+
+const Comment = ({ userAvatar, createdAt, comment, username, likes }) => {
+  const [liked, setLiked] = useState(false)
+
+  return (
+    <>
+      <Flex gap={4} py={2} my={2} w={'full'}>
+        <Avatar src={userAvatar} size={'sm'} />
+        <Flex
+          gap={1}
+          w={'full'}
+          flexDirection={'column'}
+          border={'1px solid gray'}
+          padding={2}
+        >
+          <Flex
+            w={'full'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+          >
+            <Text fontSize="sm" fontWeight="bold">
+              {username}
+            </Text>
+            <Flex gap={2} alignItems={'center'}>
+              <Text fontSize={'sm'} color={'gray.light'}>
+                {createdAt}
+              </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
+          <Text>{comment}</Text>
+          <Flex gap={4} alignItems={'center'}>
+            {liked ? (
+              <AiFillHeart
+                className="w-6 h-6 cursor-pointer"
+                style={{ color: 'red' }}
+                onClick={() => setLiked(false)}
+              />
+            ) : (
+              <AiOutlineHeart
+                className="w-6 h-6 cursor-pointer"
+                onClick={() => setLiked(true)}
+              />
+            )}
+
+            <FaRegComment className="w-6 h-6  cursor-pointer" />
+          </Flex>
+          <Text fontSize={'sm'} color={'gray.light'}>
+            {likes + (liked ? 1 : 0)} likes
+          </Text>
+        </Flex>
+      </Flex>
+      <Divider />
+    </>
+  )
+}
+
+export default Comment
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,200 @@
+/* eslint-disable react/prop-types */
+import { Avatar } from '@chakra-ui/avatar'
+import { Image } from '@chakra-ui/image'
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { BsThreeDots } from 'react-icons/bs'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+import { FaRegComment } from 'react-icons/fa6'
+import { BiRepost } from 'react-icons/bi'
+import { PiShareFat } from 'react-icons/pi'
+
+import { useState } from 'react'
+import Comment from '../components/Comments/Comment'
+
 const PostPage = () => {
-  return <div>PostPage</div>
+  const [liked, setLiked] = useState(false)
+  const [isTabActive, setIsTabActive] = useState('comments')
+
+  return (
+    <>
+      <Box gap={3} mb={4} py={5} bg="white" color="black" px={4}>
+        <Flex flexDirection={'row'} alignItems={'center'} gap={4} mb={2}>
+          <Avatar size="md" name="Fiki Aprian" src="/fiki1.jpg" />
+          <Flex justifyContent={'space-between'} w={'full'}>
+            <Flex w={'full'} alignItems={'center'}>
+              <Text fontSize={'sm'} fontWeight={'bold'}>
+                fikiap23
+              </Text>
+              <Image src="/verified.png" w={4} h={4} ml={1} />
+              <Text
+                fontSize={'sm'}
+                color={'blue.300'}
+                ml={2}
+                cursor={'pointer'}
+                textDecoration={'none'}
+                _hover={{ textDecoration: 'underline' }}
+              >
+                Follow
+              </Text>
+            </Flex>
+
+            <Flex gap={4} alignItems={'center'}>
+              <Text fontStyle={'sm'} color={'gray.light'}>
+                1d
+              </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex flex={1} flexDirection={'column'} gap={2}>
+          <Text
+            fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}
+            fontWeight={'bold'}
+          >
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Text>
+
+          <Box
+            borderRadius={6}
+            overflow={'hidden'}
+            border={'1px solid'}
+            borderColor={'gray.light'}
+          >
+            <Image src={'/post1.png'} alt={''} w={'full'} />
+          </Box>
+          <Text fontSize={{ base: 'sm', md: 'md' }}>lorem ipsum</Text>
+          <Box w={'full'} h={0.1} bg={'gray.light'}></Box>
+          <Flex gap={2} alignItems={'center'} justifyContent={'space-between'}>
+            <Flex gap={4} alignItems={'center'}>
+              {/* likes */}
+              {isTabActive === 'likes' ? (
+                <Flex
+                  onClick={() => {
+                    setIsTabActive('likes')
+                  }}
+                  cursor={'pointer'}
+                  color={'red'}
+                  textDecoration={'underline'}
+                >
+                  {liked ? (
+                    <AiFillHeart
+                      className="w-6 h-6 cursor-pointer"
+                      style={{ color: 'red' }}
+                      onClick={() => {
+                        setLiked(false)
+                      }}
+                    />
+                  ) : (
+                    <AiOutlineHeart
+                      className="w-6 h-6 cursor-pointer"
+                      onClick={() => {
+                        setLiked(true)
+                      }}
+                    />
+                  )}
+                  <Text>{200 + (liked ? 1 : 0)}</Text>
+                </Flex>
+              ) : (
+                <Flex
+                  onClick={() => {
+                    setIsTabActive('likes')
+                  }}
+                  cursor={'pointer'}
+                >
+                  {liked ? (
+                    <AiFillHeart
+                      className="w-6 h-6 cursor-pointer"
+                      style={{ color: 'red' }}
+                      onClick={() => {
+                        setLiked(false)
+                      }}
+                    />
+                  ) : (
+                    <AiOutlineHeart
+                      className="w-6 h-6 cursor-pointer"
+                      onClick={() => {
+                        setLiked(true)
+                      }}
+                    />
+                  )}
+                  <Text>200</Text>
+                </Flex>
+              )}
+              {/* end likes */}
+
+              {/* comments */}
+              {isTabActive === 'comments' ? (
+                <Flex
+                  onClick={() => {
+                    setIsTabActive('comments')
+                  }}
+                  cursor={'pointer'}
+                  color={'green'}
+                  textDecoration={'underline'}
+                >
+                  <FaRegComment className="w-6 h-6  cursor-pointer" />
+                  <Text>200</Text>
+                </Flex>
+              ) : (
+                <Flex
+                  onClick={() => {
+                    setIsTabActive('comments')
+                  }}
+                  cursor={'pointer'}
+                >
+                  <FaRegComment className="w-6 h-6  cursor-pointer" />
+                  <Text>200</Text>
+                </Flex>
+              )}
+
+              {/* end comments */}
+
+              {/* reposts */}
+              {isTabActive === 'reposts' ? (
+                <Flex
+                  onClick={() => {
+                    setIsTabActive('reposts')
+                  }}
+                  cursor={'pointer'}
+                  color={'blue'}
+                  textDecoration={'underline'}
+                >
+                  <BiRepost className="w-6 h-6  cursor-pointer" />
+                  <Text>200</Text>
+                </Flex>
+              ) : (
+                <Flex
+                  onClick={() => {
+                    setIsTabActive('reposts')
+                  }}
+                  cursor={'pointer'}
+                >
+                  <BiRepost className="w-6 h-6  cursor-pointer" />
+                  <Text>200</Text>
+                </Flex>
+              )}
+              {/* end reposts */}
+              {/* share */}
+              <Flex cursor={'pointer'}>
+                <PiShareFat className="w-6 h-6  cursor-pointer" />
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Box w={'full'} h={0.1} bg={'gray.light'}></Box>
+        {isTabActive === 'comments' && (
+          <Comment
+            userAvatar={'/fiki1.jpg'}
+            username={'fikiap23'}
+            comment={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+            likes={200}
+            key={1}
+          />
+        )}
+      </Box>
+    </>
+  )
 }
 
 export default PostPage
