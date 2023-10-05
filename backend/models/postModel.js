064ca71484586b0325ebbf71edfe80860430df4b
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-05 14:12:32 +0700

    [CREATE] userModel & postModel schema

---
+++ backend/models/postModel.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema(
+  {
+    postedBy: {
+      // id akan di generate oleh mongoose
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    text: {
+      type: String,
+      maxLength: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      // array of user ids
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'User',
+      default: [],
+    },
+    replies: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        userProfilePic: {
+          type: String,
+        },
+        username: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Post = mongoose.model('Post', postSchema)
+
+export default Post
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './db/connectDB.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
