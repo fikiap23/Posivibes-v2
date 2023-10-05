d896fa28d0b65267f835b69b277034d718b49772
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-05 08:00:45 +0700

    [CREATE] suggested user component

--- frontend/src/components/Rightbar/Rightbar.jsx
+++ frontend/src/components/Rightbar/Rightbar.jsx
@@ -1,22 +1,23 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import SuggestedUser from './SuggestedUser'
 
 const Rightbar = () => {
   return (
-    <Flex
-      w={'30%'}
-      gap={4}
-      direction={'column'}
-      className="sticky top-0 h-[100vh] hidden lg:block"
-    >
-      <Text fontSize={20} fontWeight={'bold'} my={2}>
-        Suggested User
-      </Text>
-      <SuggestedUser />
-      <SuggestedUser />
-      <SuggestedUser />
-      <SuggestedUser />
-    </Flex>
+    <Box className="hidden lg:block" w={'30%'}>
+      <Flex
+        gap={4}
+        direction={'column'}
+        className="sticky top-0 h-[100vh] w-full"
+      >
+        <Text fontSize={20} fontWeight={'bold'} my={2}>
+          Suggested User
+        </Text>
+        <SuggestedUser />
+        <SuggestedUser />
+        <SuggestedUser />
+        <SuggestedUser />
+      </Flex>
+    </Box>
   )
 }
 
