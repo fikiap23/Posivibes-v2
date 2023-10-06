5644a9c44d7acc271e20ca99827ef5f59eb32b61
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-06 19:49:29 +0700

    [ADD] middleware for protectRoute

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,4 +88,38 @@ const logoutUser = (req, res) => {
     console.log('Error in logout: ', err.message)
   }
 }
-export { signupUser, loginUser, logoutUser }
+
+const followUnFollowUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const userToModify = await User.findById(id)
+    const currentUser = await User.findById(req.user._id)
+
+    if (id === req.user._id.toString())
+      return res
+        .status(400)
+        .json({ error: 'You cannot follow/unfollow yourself' })
+
+    if (!userToModify || !currentUser)
+      return res.status(400).json({ error: 'User not found' })
+
+    const isFollowing = currentUser.following.includes(id)
+
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } })
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } })
+      res.status(200).json({ message: 'User unfollowed successfully' })
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } })
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } })
+      res.status(200).json({ message: 'User followed successfully' })
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+    console.log('Error in followUnFollowUser: ', err.message)
+  }
+}
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import {
+  followUnFollowUser,
   loginUser,
   logoutUser,
   signupUser,
 } from '../controllers/userController.js'
+import protectRoute from '../middlewares/protectRoute.js'
 
 const router = express.Router()
 
 router.post('/signup', signupUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
+router.post('/follow/:id', protectRoute, followUnFollowUser) // Toggle state(follow/unfollow)
 
 export default router
