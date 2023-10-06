35ce6016fadde8f021efccf05c4ffdc45f8a2381
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-06 20:15:25 +0700

    [ADD] api for follow&unfollow

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -16,7 +16,7 @@ const protectRoute = async (req, res, next) => {
     next()
   } catch (err) {
     res.status(500).json({ message: err.message })
-    console.log('Error in signupUser: ', err.message)
+    console.log('Error in protectRoute: ', err.message)
   }
 }
 
