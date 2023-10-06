d4c9af72dcc4f803ec74dab6e43dab0ac0d63703
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-06 23:27:41 +0700

    [ADD] api for posts

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js'
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
+import postRoutes from './routes/postRoutes.js'
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ app.use(cookieParser())
 
 // Routes
 app.use('/v1/api/users', userRoutes)
+app.use('/v1/api/posts', postRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server started at  http://localhost:${PORT}`)
