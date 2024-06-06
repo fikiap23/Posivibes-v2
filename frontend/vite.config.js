b7abd80dc3990952a4b48af44bafc8b82e567c86
fikiaprian23 <fikiaprian23@gmail.com>
2024-06-06 23:55:10 +0700

    change api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Get rid of the CORS error
     proxy: {
       '/v1': {
-        target: 'http://localhost:3000',
+        target: 'https://posivibes-v2.vercel.app',
         changeOrigin: true,
         secure: false,
       },
