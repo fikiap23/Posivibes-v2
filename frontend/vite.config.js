87206ad83c88563cdeb0d0cf08c516628bb610b2
fikiaprian23 <fikiaprian23@gmail.com>
2024-06-07 00:12:47 +0700

    tes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    host: '0.0.0.0',
+    port: 3000,
     // Get rid of the CORS error
     proxy: {
       '/v1': {
