339551b14b48374b9435946789fc93091eb55a77
fikiaprian23 <fikiaprian23@gmail.com>
2024-07-07 16:11:33 +0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { apiUrl } from './src/utils/baseURL'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000,
-    // Get rid of the CORS error
-    proxy: {
-      '/v1': {
-        target: apiUrl,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
 })
